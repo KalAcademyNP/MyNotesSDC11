e98c97c93ccecda96e05a484641710f06d58758e
Kal <info@kalacademy.org>
2021-10-03 09:30:36 -0700

    Initial commit

---
+++ MyNotes/MyNotes/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyNotes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NotesPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MyNotes/MyNotes/NotesPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyNotes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotesPage : ContentPage
+    {
+        string fileName = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), 
+            "notes.txt");
+        public NotesPage()
+        {
+            InitializeComponent();
+            if (File.Exists(fileName))
+            {
+                editor.Text = File.ReadAllText(fileName);
+            }
+        }
+
+        private void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
